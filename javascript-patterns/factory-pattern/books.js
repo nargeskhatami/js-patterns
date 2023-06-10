2b119efe220f7f00104e2ec2e5933544689e0a66
Narges <nargeskhatami.h@gmail.com>
2023-06-10 21:50:34 +0330

    add factory pattern example

---
+++ javascript-patterns/factory-pattern/books.js
@@ -0,0 +1,10 @@
+const createBook = (title, author, isbn) => ({
+  title,
+  author,
+  isbn,
+});
+export const book1 = createBook("Harry Potter", "JK Rowling", "AB123");
+export const book2 = createBook("The Great Gatsby", "F. Scott Fitzgerald", "CD456");
+export const book3 = createBook("Moby-Dick", "Herman Melville", "EF789");
+export const book4 = createBook("Harry Potter", "JK Rowling", "AB123");
+export const book5 = createBook("The Great Gatsby", "F. Scott Fitzgerald", "CD456");
